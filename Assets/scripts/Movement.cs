b21f2c0d8c24bcf4518657aaad597101c42fa0f4
Lavr <lavr3x@rambler.ru>
2016-10-22 15:01:41 +0300

    updateVersion
    start trying unity

---
+++ Assets/scripts/Movement.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(Input.GetAxis("Horizontal") * 0.1f, 0.0f, Input.GetAxis("Vertical") * 0.1f));
+        transform.Rotate(new Vector3(0, 1, 0), Input.GetAxis("Mouse X"));
+    }
+}
